ac9310e1594f9c35e1e5031c5f6322dcf9e789c3
alokkusingh <alok.ku.singh@gmail.com>
2024-08-18 20:04:25 +0530

    Parser exception handling

--- src/main/java/com/alok/email/parser/impl/TransactionParser.java
+++ src/main/java/com/alok/email/parser/impl/TransactionParser.java
@@ -4,7 +4,11 @@ import com.alok.email.model.impl.TransactionEmail;
 import com.alok.email.parser.Parser;
 import com.alok.email.parser.ParserUtils;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.UUID;
+
+@Slf4j
 public class TransactionParser implements Parser {
 
     @Getter
@@ -12,13 +16,30 @@ public class TransactionParser implements Parser {
 
     @Override
     public TransactionEmail parseEmail(final String senderEmail, final String subject, final String content) {
+        try {
+            return TransactionEmail.builder()
+                    .id(UUID.randomUUID().toString())
+                    .subject(subject)
+                    .email(senderEmail)
+                    .content(content)
+                    .bank(ParserUtils.getTransactionType(senderEmail, subject).name())
+                    .amount(ParserUtils.getAmountRetriever(senderEmail, subject).retrieve(content))
+                    .description(ParserUtils.getRemarksRetriever(senderEmail, subject).retrieve(content))
+                    .timestamp(ParserUtils.getTransactionDateRetriever(senderEmail, subject).retrieve(content))
+                    .build();
+        } catch (Exception ex) {
+            // TODO: persist senderEmail/subject/content for future reference.
+            // the same can be used to reprocessed again after fixing the issue.
+            ex.printStackTrace();
+            log.error(ex.getMessage());
+        }
+
         return TransactionEmail.builder()
+                .id(UUID.randomUUID().toString())
                 .subject(subject)
                 .email(senderEmail)
                 .content(content)
-                .amount(ParserUtils.getAmountRetriever(senderEmail, subject).retrieve(content))
-                .description(ParserUtils.getRemarksRetriever(senderEmail, subject).retrieve(content))
-                .timestamp(ParserUtils.getTransactionDateRetriever(senderEmail, subject).retrieve(content))
+                .bank(ParserUtils.getTransactionType(senderEmail, subject).name())
                 .build();
     }
 }
