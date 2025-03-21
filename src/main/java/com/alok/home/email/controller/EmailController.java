b47ac74f9142c96e70bc071002615ce67ca60a02
alokkusingh <alok.ku.singh@gmail.com>
2025-03-21 19:52:22 +0530

    Added send email POST method

--- src/main/java/com/alok/home/email/controller/EmailController.java
+++ src/main/java/com/alok/home/email/controller/EmailController.java
@@ -2,9 +2,8 @@ package com.alok.home.email.controller;
 
 import com.alok.home.email.service.EmailService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import com.alok.home.commons.dto.EmailRequest;
 
 @RequestMapping()
 @RestController
@@ -17,10 +16,20 @@ public class EmailController {
     }
 
     @GetMapping(value = "/send")
-    public ResponseEntity<Void> sendEmail() {
+    public ResponseEntity<Void> sendEmail(
+            @RequestParam(name = "to", required = false, defaultValue = "alok.ku.singh@gmail.com") String to,
+            @RequestParam(name = "subject", required = false, defaultValue = "Mail from Home Stack") String subject,
+            @RequestParam(name = "body", required = false, defaultValue = "Test mail") String body
+    ) {
 
-        emailService.sendSimpleMessage("alok.ku.singh@gmail.com", "Mail from Home Stack", "This is first mail");
+        emailService.sendSimpleMessage(to, subject, body);
+        return ResponseEntity.ok().build();
+    }
 
+    @PostMapping(value = "/send")
+    public ResponseEntity<Void> sendEmailPost(@RequestBody() EmailRequest emailRequest) {
+
+        emailService.sendSimpleMessage(emailRequest.to(), emailRequest.subject(), emailRequest.body());
         return ResponseEntity.ok().build();
     }
 }
